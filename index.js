7594c2ddc234a90efd2fc455eb0bc548233f5668
ariiiman <aristide.benoist@gmail.com>
2017-06-15 16:29:22 +0200

    4.2.3

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /**
  * Skylake
  *
- * Version   →  4.2.2
+ * Version   →  4.2.3
  * Github    →  https://github.com/ariiiman/skylake
  * License   →  http://opensource.org/licenses/MIT
  * Author    →  Copyright © 2017 Aristide Benoist
  * Website   →  www.aristidebenoist.com
- * Date      →  May 28, 2017
+ * Date      →  Jun 15, 2017
  */
 undefined
